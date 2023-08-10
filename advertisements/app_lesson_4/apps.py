8d0adb788f4fa731831fed2b1413f8ba7e9e3788
ssdevvv <salavatsibgatov@gmail.com>
2023-08-10 20:53:07 +0500

    Пятый коммит

--- advertisements/app_lesson_4/apps.py
+++ advertisements/app_lesson_4/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class AppLesson4Config(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'app_lesson_4'
+    verbose_name = "Объявления"
--- advertisements/app_lesson_4/models.py
+++ advertisements/app_lesson_4/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Advertisement(models.Model):
+    # Другие поля модели
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def last_updated(self):
+        if self.updated_at.date() == timezone.now().date():
+            return f"<span style='color: #FF0000'>Сегодня в {self.updated_at.time()}</span>"
+        else:
+            return self.updated_at
+
+    last_updated.allow_tags = True
+    last_updated.short_description = "Последнее обновление"
